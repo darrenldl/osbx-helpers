0e3fcc33ce3a1b404ecbc53b536e44661e5169ea
Darren Ldl <darrenldldev@gmail.com>
2017-08-23 21:07:08 +1000

    Added show_all.sh

---
+++ scripts/show_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILE=$1
+
+SKIP_TO=0
+
+while true; do
+  output=$(osbx show $FILE --skip-to $SKIP_TO)
+
+  SKIP_TO=$(echo $output | grep -Po "(?<=Found at byte : )\d+")
+
+  if [[ $SKIP_TO == "" ]]; then
+    break
+  fi
+
+  SKIP_TO=$[$SKIP_TO + 128]
+
+  echo "$output"
+  echo ""
+done
